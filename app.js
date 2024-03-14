7dff567cb0f70f044d99f0ec5285695bb548e576
Boy345 <42969697+PanupongSoongwiriya@users.noreply.github.com>
2024-03-14 23:15:13 +0700

    Lecture 12
    Nodemon และ การจัดการ Environment Variable

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 const express = require('express');
+const path = require('path');
 
 //Other setting for show detail when call web
 const debug = require('debug')('app');
 const morgan = require('morgan');
 
-const path = require('path');
-
 const app = express();
-const port = 2005;
+const PORT = process.env.PORT;
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, "/public/")));
 
 app.get("/", (req, res) => {
-    res.send("Hello world 333");
+    res.send("Hello world");
 });
 
-app.listen(port, ()=>{
-    debug("Listening on port %d", port);
+app.listen(PORT, ()=>{
+    debug("Listening on PORT %d", PORT);
 })
