4b9cf92e0b9990a09a8d33f0db4dd9cf7ff95ee6
Boy345 <42969697+PanupongSoongwiriya@users.noreply.github.com>
2024-03-14 22:54:25 +0700

    Lecture 8

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+
+//Other setting for show detail when call web
 const debug = require('debug')('app');
 const morgan = require('morgan');
+
+const path = require('path');
+
 const app = express();
-const port = 3000;
+const port = 2005;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, "/public/")));
 
 app.get("/", (req, res) => {
     res.send("Hello world 333");
